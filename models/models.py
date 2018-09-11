ada0e32008694ed446a98725abf5d4e1375a64f5
ligiger <l.giger@getronic.ch>
2018-09-11 21:03:22 +0200

    q

--- models/models.py
+++ models/models.py
@@ -97,13 +97,6 @@ class q_note(models.Model):
     description = fields.Html('Beschreibung')
     analysis = fields.Html('Problemanalyse')
 
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
-
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
